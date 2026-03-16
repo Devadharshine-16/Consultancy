ae1a6bf5785981a7f53db6d796e534a8d1075958
devadharshine16 <devadharshinea.23it@kongu.edu>
2026-03-17 01:55:36 +0530

    Initial commits

--- propzen-backend/server.js
+++ propzen-backend/server.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:5174",
+  origin: process.env.CLIENT_URL || "https://vazhvidam.vercel.app",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
